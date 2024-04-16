810d4df59f2e15de24cbce4177e83199e1207542
Osama Youssef <73332227+Osama-Yusuf@users.noreply.github.com>
2024-04-16 07:53:36 +0200

    Update k8s_img_valid.sh

--- k8s_img_valid.sh
+++ k8s_img_valid.sh
@@ -1,45 +1,61 @@
 #!/bin/bash
 
+# Retrieve a list of namespaces from Kubernetes and store them in an array
 namespaces=$(kubectl get namespace -o=jsonpath='{.items[*].metadata.name}')
 IFS=' ' read -r -a namespaces_array <<< "$namespaces"
 
-echo "\nPlease select a namespace "
+# Prompt the user to select a namespace from the list
+echo "Please select a namespace "
 
+# Provide a selection mechanism for the namespace array
 select chosen_namespace in "${namespaces_array[@]}"; do
+    # Check if a namespace was selected
     if [[ -n "$chosen_namespace" ]]; then
-        echo -e "\nyou have selected ns: $chosen_namespace\n"
-        break
+        echo -e "\nYou have selected $chosen_namespace namespace\n"
+        break # Break the loop if selection is valid
     else
-        echo "invalid"
+        echo "invalid" # Indicate invalid selection
     fi
 done
 
-microservices=$(kubectl get deploy -n $chosen_namespace -o=jsonpath='{.items[*].metadata.name}')
+# Retrieve a list of deployments from Kubernetes and store them in an array
+microservices=$(kubectl get deployments -o=jsonpath='{.items[*].metadata.name}')
 IFS=' ' read -r -a microservices_array <<< "$microservices"
 
+# Process each microservice to cut by "-"
 for i in "${!microservices_array[@]}"; do
+    # Use Bash substring removal instead of cut for better integration in the script
     microservices_array[i]="${microservices_array[i]%%-*}"
 done
 
+# Prompt the user to select a namespace from the list
 echo -e "\nPlease select a microservice "
 
+# Provide a selection mechanism for the namespace array
 select microservice in "${microservices_array[@]}"; do
+    # Check if a microservice was selected
     if [[ -n "$microservice" ]]; then
-        echo -e "\nYou have selected: $microservice\n"
-        break
+        echo -e "\nYou have selected $microservice microservice\n"
+        break # Break the loop if selection is valid
     else
-        echo "invalid"
+        echo "invalid" # Indicate invalid selection
     fi
 done
 
+# Initialize a counter for pods
 counter=1
+# Retrieve a list of pods from the selected namespace that match the microservice name
 microservice_pods=$(kubectl get pods -n $chosen_namespace | grep $microservice | awk '{print $1}')
 
+# Loop through each pod associated with the microservice
 for microservice_pod in $microservice_pods
 do
+    # Extract the microservice ID from the pod name
     microservice_id=$(echo $microservice_pod | cut -d "-" -f5)
-    echo "$microservice Pod No.$counter with ID of $microservice_id validation output:"
-    validation_output=$(kubectl describe pod $microservice_pod -n $chosen_namespace | grep -i image | grep -vi "image id"| grep release: | cut -d ":" -f4)
+    echo "Microservice Pod No.$counter with ID of $microservice_id validation output:"
+    # Describe the pod and filter for image id and release information, store in variable
+    validation_output=$(kubectl describe pod $microservice_pod -n $chosen_namespace | grep -i image | grep -vi "image id" | grep release: | cut -d " " -f4)
+    # Print the validation output
     echo -e "$validation_output\n"
-    ((counter++))
+    ((counter++)) # Increment the counter
 done
