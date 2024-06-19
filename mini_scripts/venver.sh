ad3d86cf1a53c90adfa11b1633831251915b9ad5
Administrator <osama9mohamed5@gmail.com>
2024-06-19 15:58:21 +0300

    updated venver to copy the cmd in clipboard

--- mini_scripts/venver.sh
+++ mini_scripts/venver.sh
@@ -3,14 +3,33 @@
 # Define the name of the virtual environment
 venv_name="my_venv"
 
+get_clipboard_command() {
+    case "$(uname)" in
+        "Linux")
+            echo "xclip -selection clipboard"  # Debian-based systems like Ubuntu
+            ;;
+        "Darwin")
+            echo "pbcopy"  # macOS uses pbcopy
+            ;;
+        *)
+            echo "Unsupported OS for clipboard copying"
+            ;;
+    esac
+}
+
 # Function to create the virtual environment
 create_venv() {
     # Create the virtual environment
     python3 -m venv "$venv_name"
-    
-    # Activate the virtual environment
-    echo "Execute the following command to activate the virtual environment:\n"
-    echo "source "$venv_name/bin/activate""
+    echo "Virtual environment '$venv_name' created."
+    clipboard_cmd=$(get_clipboard_command)
+    if [ "$clipboard_cmd" = "Unsupported OS for clipboard copying" ]; then
+        echo "Clipboard copying is not supported on this OS."
+    else
+        echo "source "$venv_name/bin/activate"" | $clipboard_cmd
+        echo "Source CMD copied to clipboard."
+        echo "Please paste it in your terminal to activate the virtual environment."
+    fi
 }
 
 # Function to remove the virtual environment
